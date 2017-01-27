c628cca95c41826f74fd04714dfce3cdf3ea7ec6
Balaji Subramaniam <vidya.balaji@Balajis-MacBook-Pro.local>
2017-01-26 20:37:43 -0800

    Initial Commit

---
+++ TestCPP/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  TestCPP
+//
+//  Created by Balaji Subramaniam on 1/26/17.
+//  Copyright © 2017 Balaji Subramaniam. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
